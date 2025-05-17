cffa7c2402cc935b8f2f6353055ebd2b466a979d
lOkY <102552151+IvanIsay@users.noreply.github.com>
2025-05-16 18:19:01 -0600

    variables

--- practica1/sintaxis.py
+++ practica1/sintaxis.py
@@ -9,5 +9,27 @@ en python """
 
 
 #2. strings
-print("hola soy una cadena")
-print('yo soy otra')
+#print('yo soy otra')
+
+#variable1= "hola soy una cadena"
+
+#print(len(variable1)) 
+#print(variable1[2:5])
+#print(variable1[2:])
+#print(variable1[:5])
+
+#3. Variables
+
+x= "ivan isaay"
+x= 4
+x= 5.76
+print(x)
+
+x= int(3)
+y= float(3)
+z=str(3)
+
+print(x,y,z)
+print(type(x))
+print(type(y))
+print(type(z))
