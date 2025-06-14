5c0c41506128f9ac5f0fb3b1b45aa682a5631c8b
Ivan Guerra <102552151+IvanIsay@users.noreply.github.com>
2025-06-13 18:36:34 -0600

    Agregamos la ruta para probar la conexion a la BD

--- flaskProyect/app.py
+++ flaskProyect/app.py
@@ -1,7 +1,28 @@
 
-from flask import Flask
+from flask import Flask,jsonify
+from flask_mysqldb import MySQL
+import MySQLdb
 
 app= Flask(__name__)
+
+app.config['MYSQL_HOST']="localhost"
+app.config['MYSQL_USER']="root"
+app.config['MYSQL_PASSWORD']=""
+app.config['MYSQL_DB']="dbFlask"
+#app.config['MYSQL_PORT']=3306 //Usar solo en cambio de puerto
+
+mysql= MySQL(app)
+
+#ruta para probar la conexion a MYSQL
+@app.route('/DBCheck')
+def DB_check():
+    try:
+        cursor= mysql.connection.cursor()
+        cursor.execute('Select 1')
+        return jsonify( {'status':'ok','message':'Conectado con exito'} ),200   
+    except MySQLdb.MySQLError as e:
+        return jsonify( {'status':'error','message':str(e)} ),500
+
 
 #ruta simple
 @app.route('/')
