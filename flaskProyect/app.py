fc3e56327d574ab2e57c6ca28de7b20e3b94b5f9
Ivan Guerra <102552151+IvanIsay@users.noreply.github.com>
2025-06-25 19:19:20 -0600

    Update y Delete

--- flaskProyect/app.py
+++ flaskProyect/app.py
@@ -18,9 +18,133 @@ mysql= MySQL(app)
 #ruta de inicio
 @app.route('/')
 def home():
-    return render_template('formulario.html', errores={})
+    try:
+        cursor= mysql.connection.cursor()
+        cursor.execute('SELECT * FROM tb_albums WHERE state= 1')
+        consultaTodo= cursor.fetchall()
+        return render_template('formulario.html', errores={},albums=consultaTodo)
+    
+    except Exception as e:
+        print('Error al consultar todo: '+ str(e))
+        return render_template('formulario.html', errores={},albums=[])
+        
+    finally:
+        cursor.close()
+        
+        
+#ruta de detalle
+@app.route('/detalle/<int:id>')
+def detalle(id):
+    try:
+        cursor= mysql.connection.cursor()
+        cursor.execute('SELECT * FROM tb_albums WHERE id=%s',(id,))
+        consultaId= cursor.fetchone()
+        return render_template('consulta.html',album=consultaId)
+    
+    except Exception as e:
+        print('Error al consultar por id : '+ str(e))
+        return redirect(url_for('home'))
+        
+    finally:
+        cursor.close()
+        
 
-#ruta de conusta
+#ruta para editar registro
+@app.route('/editaAlbum/<int:id>')
+def editar(id):
+    try:
+        cursor= mysql.connection.cursor()
+        cursor.execute('SELECT * FROM tb_albums WHERE id=%s',(id,))
+        consultaId= cursor.fetchone()
+        return render_template('formUpdate.html',album=consultaId)
+    
+    except Exception as e:
+        print('Error al consultar por id : '+str(e))
+        return redirect(url_for('home'))
+        
+    finally:
+        cursor.close()   
+
+
+#ruta confirmar Delete
+@app.route('/confirmaDel/<int:id>')
+def confirma(id):
+    try:
+        cursor= mysql.connection.cursor()
+        cursor.execute('SELECT * FROM tb_albums WHERE id=%s',(id,))
+        consultaId= cursor.fetchone()
+        return render_template('confirmDel.html',album=consultaId)
+    
+    except Exception as e:
+        print('Error al consultar por id : '+str(e))
+        return redirect(url_for('home'))
+        
+    finally:
+        cursor.close()   
+        
+        
+#ruta para el Actualizar
+@app.route('/ActualizarAlbum/<int:id>',methods=['POST'])
+def actualizar(id):
+    
+    #lista de errores
+    errores={}
+    
+    #obtener los datos a insertar
+    tituloV= request.form.get('txtTitulo','').strip()
+    artistaV= request.form.get('txtArtista','').strip()
+    anioV= request.form.get('txtAnio','').strip()
+    
+    if not tituloV:
+        errores['txtTitulo']= 'Nombre del album Obligatorio'
+    if not artistaV:
+        errores['txtArtista']= 'Artista es Obligatorio'
+    if not anioV:
+        errores['txtAnio']= 'Año es Obligatorio'
+    elif not anioV.isdigit() or int(anioV)< 1800 or int(anioV) > 2100 :
+        errores['txtAnio']= 'Ingresa una año Valido'       
+        
+    if not errores:
+        try:
+            cursor= mysql.connection.cursor()
+            cursor.execute('update tb_albums set album= %s, artista= %s, anio=%s where id= %s', (tituloV,artistaV,anioV, id))
+            mysql.connection.commit()
+            flash('Album Actulizado en BD')
+            return redirect(url_for('home'))
+        
+        except Exception as e:
+            mysql.connection.rollback()
+            flash('Algo fallo: '+ str(e))
+            return redirect(url_for('home'))
+        
+        finally: 
+            cursor.close()
+    
+    return render_template('formulario.html',errores=errores)
+       
+
+#ruta para el Eliminar
+@app.route('/EliminarAlbum/<int:id>',methods=['POST'])
+def softDel(id):
+    
+    try:
+        cursor= mysql.connection.cursor()
+        cursor.execute('update tb_albums set state=%s where id= %s', (0, id))
+        mysql.connection.commit()
+        flash('Album Eliminado en BD')
+        return redirect(url_for('home'))
+        
+    except Exception as e:
+        mysql.connection.rollback()
+        flash('Algo fallo: '+ str(e))
+        return redirect(url_for('home'))
+        
+    finally: 
+        cursor.close()
+
+
+
+#ruta de para abrir detalle
 @app.route('/consulta')
 def consulta():
     return render_template('consulta.html')
